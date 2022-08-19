d1ded342d087cf19d5693ce7fed185424c5a8f77
Kodak678 <95473076+Kodak678@users.noreply.github.com>
2022-08-19 18:40:32 +0100

    Add files via upload

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,5 @@
+from email import message
+from turtle import rt
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 import chess
@@ -12,20 +14,44 @@ board = chess.Board()
 stockfish = Stockfish(path="static\stockfish_15_win_x64_avx2\stockfish_15_x64_avx2.exe")
 engine = chess.engine.SimpleEngine.popen_uci(r"static\stockfish_15_win_x64_avx2\stockfish_15_x64_avx2.exe")
 
+board.has_legal_en_passant()
+bool(chess.BB_H1)
+bool(chess.BB_A1)
+bool(chess.BB_A8)
+bool(chess.BB_H8)
+board.is_checkmate()
 # Create your views here.
+def isValid(move):
+    try:
+        board.push_san(move)
+        return True
+    except ValueError:
+        return False
+
 
 def boardStates(request):
-    message = "Sent back Python response"
-    return JsonResponse({"response": message,
-    "fish": "hiii",
-    "tryMe": "tryme"}, status = 200)
+    request.GET.get(message)
+    legalEnPassant =  board.has_legal_en_passant()
+    whiteKingSide = bool(chess.BB_H1)
+    whiteQueenSide = bool(chess.BB_A1)
+    blackQueenSide = bool(chess.BB_A8)
+    blackKingSide = bool(chess.BB_H8)
+    print(legalEnPassant)
+    print(whiteKingSide)
+    print(whiteQueenSide)
+    print(blackQueenSide)
+    print(blackKingSide)
+    return JsonResponse({"legalEnPassant": legalEnPassant,
+    "whiteKingSide": whiteKingSide,
+    "whiteQueenSide": whiteQueenSide,
+    "blackQueenSide": blackQueenSide,
+    "blackKingSide": blackKingSide,
+     },status = 200)
 
 def validMove(request):
-    text = request.POST.get('message')
-    message = "Sent back Python response"
-    return JsonResponse({"response": message,
-    "fish": "hiii",
-    "tryMe": "tryme"}, status = 200)
+    move = request.POST.get('moveMade')
+    return JsonResponse({
+    "valid": isValid(move)}, status = 200)
     
 def load_board(request):
     return render(request, 'board.html')
