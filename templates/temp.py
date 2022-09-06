79465a7067ec54057a396b8d3f29a5be6a8b7f2a
Kodak678 <95473076+Kodak678@users.noreply.github.com>
2022-09-06 20:56:02 +0100

    Add files via upload

--- templates/temp.py
+++ templates/temp.py
@@ -11,69 +11,15 @@ board  = chess.Board()
 
 
 
-AIboard = numpy.zeros((14, 8, 8), dtype=numpy.int8)
-pawn_number = 1
-knight_number = 2
-bishop_number = 3
-rook_number = 4
-queen_number = 5
-king_number = 6
+
 
 #unravel_index converts an index such as 9 into its corresponding co ordinate for an array of given dimensions.
 #for example: in an 8 by 8 dimensional array: the co ordinates corresponding to the index 9 would be (1,1)
 #The index (1,1) was calculated by doing 1 x 8 + 1 to get 9
 
-
-
-
-for index in board.pieces(pawn_number, True):
-    position = numpy.unravel_index(index, (8, 8))  #In a board of 64, each coordinate is represented by an index numberered from 0 to 63, in order to convert this index to a coordinate: I have unravelled it
-    AIboard[pawn_number - 1][position[0]][position[1]] = 1 #The first coordinate is the number of the array (out of 12) the second coordinate and the other two are y and x coordinates respectively
-for index in board.pieces(pawn_number, False): #First the indexes of all the white pieces of the piece number are added to the numpy array: AIboard
-    position = numpy.unravel_index(index, (8, 8))
-    AIboard[pawn_number][position[0]][position[1]] = 1
-
-
-for index in board.pieces(knight_number, True):
-    position = numpy.unravel_index(index, (8, 8))
-    AIboard[knight_number][position[0]][position[1]] = 1
-for index in board.pieces(knight_number, False):
-    position = numpy.unravel_index(index, (8, 8))
-    AIboard[knight_number + 1][position[0]][position[1]] = 1
-
-
-for index in board.pieces(bishop_number, True):
-    position = numpy.unravel_index(index, (8, 8))
-    AIboard[bishop_number + 1][position[0]][position[1]] = 1
-for index in board.pieces(bishop_number, False):
-    position = numpy.unravel_index(index, (8, 8))
-    AIboard[bishop_number + 2][position[0]][position[1]] = 1
-
-
-for index in board.pieces(rook_number, True):
-    position = numpy.unravel_index(index, (8, 8))
-    AIboard[rook_number + 2][position[0]][position[1]] = 1
-for index in board.pieces(rook_number, False):
-    position = numpy.unravel_index(index, (8, 8))
-    AIboard[rook_number + 3][position[0]][position[1]] = 1
-
-
-for index in board.pieces(queen_number, True):
-    position = numpy.unravel_index(index, (8, 8))
-    AIboard[queen_number + 3][position[0]][position[1]] = 1
-for index in board.pieces(queen_number, False):
-    position = numpy.unravel_index(index, (8, 8))
-    AIboard[queen_number + 4][position[0]][position[1]] = 1
-
-
-for index in board.pieces(king_number, True):
-    position = numpy.unravel_index(index, (8, 8))
-    AIboard[king_number + 4][position[0]][position[1]] = 1
-for index in board.pieces(king_number, False):
-    position = numpy.unravel_index(index, (8, 8))
-    AIboard[king_number + 5][position[0]][position[1]] = 1
-
-
+board.push_san("e2e4")
+board.push_san("d7d5")
+print(board)
 
 letterToCoordinate = {
   'a': 0,
@@ -89,31 +35,92 @@ letterToCoordinate = {
 
 
 def coordinateToIndex(square):
-  letter = chess.square_name(square)
-  return 8 - int(letter[1]), letterToCoordinate[letter[0]]
-
-board.push_san("e2e4")
-board.push_san("d7d5")
-print(board)
+  letter = chess.square_name(square) #This function call converts the square index of a square such as 28 to its name; which for '28' would be 'e4'
+  return 8 - int(letter[1]), letterToCoordinate[letter[0]] #This part takes that name, e.g. 'e4' and convert this position to its coordinate of 4 4 using the letterToCoordinate dictionary to convert the letter
 
 
-trial = numpy.zeros((2, 8, 8), dtype=numpy.int8)
+def ConvertToAIboard(board):
+    #This program turns the chess board into something the AI can analyse better
+    AIboard = numpy.zeros((14, 8, 8), dtype=numpy.int8)
+    pawn_number = 1
+    knight_number = 2
+    bishop_number = 3
+    rook_number = 4
+    queen_number = 5
+    king_number = 6
 
-RealTurn = board.turn
-board.turn = chess.WHITE
-for move in board.generate_legal_captures():
-  y , x = coordinateToIndex(move.to_square)
-  trial[0][y][x] = 1
-
-board.turn = chess.BLACK
-for move in board.generate_legal_captures():
-  y , x = coordinateToIndex(move.to_square)
-  trial[1][y][x] = 1
-
-board.turn = RealTurn
+    for index in board.pieces(pawn_number, True):
+        position = numpy.unravel_index(index, (8, 8))  #In a board of 64, each coordinate is represented by an index numberered from 0 to 63, in order to convert this index to a coordinate: I have unravelled it
+        AIboard[pawn_number - 1][position[0]][position[1]] = 1 #The first coordinate is the number of the array (out of 12) the second coordinate and the other two are y and x coordinates respectively
+    for index in board.pieces(pawn_number, False): #First the indexes of all the white pieces of the piece number are added to the numpy array: AIboard
+        position = numpy.unravel_index(index, (8, 8))
+        AIboard[pawn_number][position[0]][position[1]] = 1
 
 
+    for index in board.pieces(knight_number, True):
+        position = numpy.unravel_index(index, (8, 8))
+        AIboard[knight_number][position[0]][position[1]] = 1
+    for index in board.pieces(knight_number, False):
+        position = numpy.unravel_index(index, (8, 8))
+        AIboard[knight_number + 1][position[0]][position[1]] = 1
 
-print(trial)
+
+    for index in board.pieces(bishop_number, True):
+        position = numpy.unravel_index(index, (8, 8))
+        AIboard[bishop_number + 1][position[0]][position[1]] = 1
+    for index in board.pieces(bishop_number, False):
+        position = numpy.unravel_index(index, (8, 8))
+        AIboard[bishop_number + 2][position[0]][position[1]] = 1
+
+
+    for index in board.pieces(rook_number, True):
+        position = numpy.unravel_index(index, (8, 8))
+        AIboard[rook_number + 2][position[0]][position[1]] = 1
+    for index in board.pieces(rook_number, False):
+        position = numpy.unravel_index(index, (8, 8))
+        AIboard[rook_number + 3][position[0]][position[1]] = 1
+
+
+    for index in board.pieces(queen_number, True):
+        position = numpy.unravel_index(index, (8, 8))
+        AIboard[queen_number + 3][position[0]][position[1]] = 1
+    for index in board.pieces(queen_number, False):
+        position = numpy.unravel_index(index, (8, 8))
+        AIboard[queen_number + 4][position[0]][position[1]] = 1
+
+
+    for index in board.pieces(king_number, True):
+        position = numpy.unravel_index(index, (8, 8))
+        AIboard[king_number + 4][position[0]][position[1]] = 1
+    for index in board.pieces(king_number, False):
+        position = numpy.unravel_index(index, (8, 8))
+        AIboard[king_number + 5][position[0]][position[1]] = 1
+
+
+
+    #The pairs of for loops create 2, two dimensional numpy arrays with ones representing the case where a piece of the 
+    #corresponding number and color is positioned on it
+
+
+
+
+    RealTurn = board.turn
+    board.turn = chess.WHITE
+    for move in board.generate_legal_captures():
+        y , x = coordinateToIndex(move.to_square)
+    AIboard[12][y][x] = 1
+
+    board.turn = chess.BLACK
+    for move in board.generate_legal_captures():
+        y , x = coordinateToIndex(move.to_square)
+    AIboard[13][y][x] = 1
+
+    board.turn = RealTurn
+
+    #The above chunk of code turns all the squares that black and white can capture a piece on and converts it into into a square index
+    #The chunck of code saves the current player turn in a temporray variable to retore to once its simulated the board from the perspective of both players
+    
+
+    return(AIboard)
 
 
